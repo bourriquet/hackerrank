26ac933fc9f70c4f271de0567d268dd1d46a2360
Lauren Anders <laurenanders92@gmail.com>
2017-03-12 17:17:21 +0000

    Create dataTypes2.py

---
+++ 30DaysOfCode/day01/dataTypes2.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+another_i = int(raw_input())
+another_d = float(raw_input())
+another_s = raw_input()
+
+# Print the sum of both integer variables on a new line.
+print i + another_i
+
+# Print the sum of the double variables on a new line.
+print d + another_d
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + another_s
