1360a5de5f281f6b628605e8407bc8d78f2de02c
Lauren Anders <laurenanders92@gmail.com>
2017-03-12 18:57:03 +0000

    Create operators.js

---
+++ 30DaysOfCode/day02/operators.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var _input = '';
+var _index = 0;
+
+process.stdin.on('data', (data) => { _input += data; });
+process.stdin.on('end', () => {
+	_input = _input.split(new RegExp('[\n ]+'));
+	main(+(_input[0]), +(_input[1]), +(_input[2]));    
+});
+
+function main(mealCost, tipPercent, taxPercent) {
+
+    var tip = 0.01 * tipPercent * mealCost;
+    var tax = 0.01 * taxPercent * mealCost;
+
+    var totalCost = Math.round(mealCost + tip + tax);
+
+    console.log("The total meal cost is " + totalCost + " dollars.");
+    
+}
