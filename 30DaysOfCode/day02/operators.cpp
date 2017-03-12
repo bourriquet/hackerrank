78e9474153b15f86b549c0c58e6e570da6449796
Lauren Anders <laurenanders92@gmail.com>
2017-03-12 18:52:10 +0000

    Create operators.cpp

---
+++ 30DaysOfCode/day02/operators.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    double mealCost;
+    int tipPercent, taxPercent;
+    
+    cin >> mealCost >> tipPercent >> taxPercent;
+    
+    double tip = 0.01 * tipPercent * mealCost;
+    double tax = 0.01 * taxPercent * mealCost;
+    
+    double totalCost = round(mealCost + tip + tax);
+    
+    cout << "The total meal cost is " << totalCost << " dollars." << endl;
+    
+    return 0;
+    
+}
