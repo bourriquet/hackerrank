54642824ac56551201e24145b4e9eedc08acb652
Lauren Anders <laurenanders92@gmail.com>
2017-03-12 22:16:08 +0000

    Create recursion.cpp

---
+++ 30DaysOfCode/day09/recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int num) {
+    if (num <= 1) {
+        return 1;
+    }
+    
+    return num * factorial(num-1);
+}
+
+int main() {
+    int N;
+    cin >> N;
+    
+    cout << factorial(N);
+    
+    return 0;
+}
