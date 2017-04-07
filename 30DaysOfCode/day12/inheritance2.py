9ac82cbc34720b96e0b8d608077de453b9f15f60
Lauren Anders <laurenanders92@gmail.com>
2017-04-07 20:00:27 +0100

    Update inheritance2.py

--- 30DaysOfCode/day12/inheritance2.py
+++ 30DaysOfCode/day12/inheritance2.py
@@ -8,27 +8,27 @@ class Person:
 		print "ID:", self.idNumber
     
 class Student(Person):
-    def __init__(self, firstName, lastName, idNumber, testScores):
+	def __init__(self, firstName, lastName, idNumber, testScores):
         Person.__init__(self, firstName, lastName, idNumber)
         self.testScores = testScores
     
-    def calculate(self):
-        scoreAvg = 0
-        for i in range(0, len(self.testScores)):
-            scoreAvg += self.testScores[i]
-        scoreAvg /= len(self.testScores)
-        
-        if (scoreAvg >= 90 and scoreAvg <= 100):
-            return 'O'
-        elif (scoreAvg >= 80 and scoreAvg < 90):
-            return 'E'
-        elif (scoreAvg >= 70 and scoreAvg < 80):
-            return 'A'
-        elif (scoreAvg >= 55 and scoreAvg < 70):
-            return 'P'
-        elif (scoreAvg >= 40 and scoreAvg < 55):
-            return 'D'
-        return 'T'
+	def calculate(self):
+		scoreAvg = 0
+		for i in range(0, len(self.testScores)):
+			scoreAvg += self.testScores[i]
+		scoreAvg /= len(self.testScores)
+
+		if (scoreAvg >= 90 and scoreAvg <= 100):
+			return 'O'
+		elif (scoreAvg >= 80 and scoreAvg < 90):
+			return 'E'
+		elif (scoreAvg >= 70 and scoreAvg < 80):
+			return 'A'
+		elif (scoreAvg >= 55 and scoreAvg < 70):
+			return 'P'
+		elif (scoreAvg >= 40 and scoreAvg < 55):
+			return 'D'
+		return 'T'
 
 line = raw_input().split()
 firstName = line[0]
@@ -36,6 +36,7 @@ lastName = line[1]
 idNum = line[2]
 numScores = int(raw_input())
 scores = map(int, raw_input().split())
+
 s = Student(firstName, lastName, idNum, scores)
 s.printPerson()
 print "Grade:", s.calculate()
