9966d61dab9ec2e50bf3dc01315aaa57d06e1d8c
Lauren Anders <laurenanders92@gmail.com>
2017-04-07 19:51:16 +0100

    Update inheritance.cpp

--- 30DaysOfCode/day12/inheritance.cpp
+++ 30DaysOfCode/day12/inheritance.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 class Person{
 	protected:
 		string firstName;
@@ -15,7 +14,7 @@ class Person{
 			this->lastName = lastName;
 			this->id = identification;
 		}
-		void printPerson(){
+		void printPerson() {
 			cout << "Name: "<< lastName << ", " << firstName << "\nID: " << id << "\n"; 
 		}
 	
@@ -25,42 +24,42 @@ class Student :  public Person {
 	private:
 		vector<int> testScores;  
 	public:
-        Student(string firstName, string lastName, int id, vector<int> scores) : Person(firstName, lastName, id) {
-            testScores = scores;
-        }
+        	Student(string firstName, string lastName, int id, vector<int> scores) : Person(firstName, lastName, id) {
+            		testScores = scores;
+        	}
     
-        char calculate() {
-            int scoreAvg = 0;
+        	char calculate() {
+          		int scoreAvg = 0;
             
-            for (int i = 0; i < testScores.size(); i++) {
-                scoreAvg += testScores[i];
-            }
-            scoreAvg /= testScores.size();
+            		for (int i = 0; i < testScores.size(); i++) {
+                		scoreAvg += testScores[i];
+            		}
+            		scoreAvg /= testScores.size();
             
-            if (scoreAvg >= 90 && scoreAvg <= 100)
-                return 'O';
-            else if (scoreAvg >= 80 && scoreAvg < 90)
-                return 'E';
-            else if (scoreAvg >= 70 && scoreAvg < 80)
-                return 'A';
-            else if (scoreAvg >= 55 && scoreAvg < 70)
-                return 'P';                
-            else if (scoreAvg >= 40 && scoreAvg < 55)
-                return 'D';                
-            return 'T';
-        }
+		    	if (scoreAvg >= 90 && scoreAvg <= 100)
+				return 'O';
+		    	else if (scoreAvg >= 80 && scoreAvg < 90)
+				return 'E';
+		    	else if (scoreAvg >= 70 && scoreAvg < 80)
+				return 'A';
+		    	else if (scoreAvg >= 55 && scoreAvg < 70)
+				return 'P';                
+		    	else if (scoreAvg >= 40 && scoreAvg < 55)
+				return 'D';                
+		    	return 'T';
+        	}
 };
 
 int main() {
 	string firstName;
-  string lastName;
+	string lastName;
 	int id;
-  int numScores;
+	int numScores;
 	cin >> firstName >> lastName >> id >> numScores;
-  vector<int> scores;
-  for(int i = 0; i < numScores; i++){
-	  int tmpScore;
-	  cin >> tmpScore;
+	vector<int> scores;
+	for(int i = 0; i < numScores; i++) {
+		int tmpScore;
+		cin >> tmpScore;
 		scores.push_back(tmpScore);
 	}
 	Student* s = new Student(firstName, lastName, id, scores);
