38bdc1801553d24b632a1eecac7ef672667c056a
Lauren Anders <laurenanders92@gmail.com>
2017-04-08 23:05:13 +0100

    Create linkedList2.py

---
+++ 30DaysOfCode/day15/linkedList2.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+class Solution:
+    def display(self,head):
+        current = head
+        while current:
+            print current.data,
+            current = current.next
+            
+    def insert(self,head,data):
+        a = Node(data)
+        
+        if (head == None):
+            head = a
+        elif (head.next == None):
+            head.next = a
+        else:
+            nextNode = head.next
+            while (nextNode.next):
+                nextNode = nextNode.next
+            nextNode.next = a
+        
+        return head
+        
+mylist= Solution()
+T=int(input())
+head=None
+
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+
+mylist.display(head);
