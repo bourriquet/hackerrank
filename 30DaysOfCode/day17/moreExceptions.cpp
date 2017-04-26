68bf753abd6c47171d4e996091711d73b4fe5b11
Lauren Anders <laurenanders92@gmail.com>
2017-04-26 21:21:57 +0100

    Update moreExceptions.cpp

--- 30DaysOfCode/day17/moreExceptions.cpp
+++ 30DaysOfCode/day17/moreExceptions.cpp
@@ -9,7 +9,7 @@ class Calculator {
     public:
       int power(int n, int p) {
           if (n < 0 || p < 0)
-              throw invalid_argument("n and p should be non-negative");
+             throw invalid_argument("n and p should be non-negative");
           return pow(n, p);
       }
 };
@@ -21,8 +21,8 @@ int main() {
     while (T-->0) {
       if (scanf("%d %d", &n, &p) == 2) {
          try {
-            int ans = myCalculator.power(n,p);
-            cout << ans << endl; 
+             int ans = myCalculator.power(n,p);
+             cout << ans << endl; 
          }
          catch (exception& e) {
              cout << e.what() << endl;
