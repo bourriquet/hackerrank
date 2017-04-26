481a677604fdba6e1e03d6c47c009d111ec1f9dd
Lauren Anders <laurenanders92@gmail.com>
2017-04-26 21:41:28 +0100

    Update moreExceptions2.py

--- 30DaysOfCode/day17/moreExceptions2.py
+++ 30DaysOfCode/day17/moreExceptions2.py
@@ -2,7 +2,7 @@ class Calculator:
     def power(self, n, p):
         if (n < 0 or p < 0):
             raise Exception("n and p should be non-negative")
-        return n**p
+        return n ** p
         
 myCalculator = Calculator()
 T = int(raw_input())
