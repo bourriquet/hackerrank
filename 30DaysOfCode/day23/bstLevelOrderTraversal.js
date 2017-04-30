8f638c0975787528bdb11ebd15099957e55cd206
Lauren Anders <laurenanders92@gmail.com>
2017-04-30 21:42:57 +0100

    Create bstLevelOrderTraversal.js

---
+++ 30DaysOfCode/day23/bstLevelOrderTraversal.js
@@ -0,0 +1,72 @@
+function Node(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+};
+
+function BinarySearchTree() {
+    this.insert = function(root, data) {
+        if (root === null) {
+            this.root = new Node(data);
+            
+            return this.root;
+        }
+        
+        if (data <= root.data) {
+            if (root.left) {
+                this.insert(root.left, data);
+            } else {
+                root.left = new Node(data);
+            }
+        } else {
+            if (root.right) {
+                this.insert(root.right, data);
+            } else {
+                root.right = new Node(data);
+            }
+        }
+        
+        return this.root;
+    };
+    
+    this.levelOrder = function(root) {
+        var n = root;
+        var q = [];
+
+        while(n) {
+            process.stdout.write(n.data + " ");
+            
+            if (n.left)
+                q.push(n.left);
+            if (n.right)
+                q.push(n.right);
+            
+            if (q.length > 0)
+                n = q.shift();
+            else
+                n = null;
+        }
+    };
+};
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var _input = "";
+
+process.stdin.on('data', function (data) {
+    _input += data;
+});
+
+process.stdin.on('end', function () {
+    var tree = new BinarySearchTree();
+    var root = null;
+    
+    var values = _input.split('\n').map(Number);
+    
+    for (var i = 1; i < values.length; i++) {
+        root = tree.insert(root, values[i]);
+    }
+    
+    tree.levelOrder(root);
+});
