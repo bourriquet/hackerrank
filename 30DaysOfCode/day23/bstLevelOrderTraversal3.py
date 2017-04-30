d76da6445fdc588b34e1fa83d2fcdc4de164c340
Lauren Anders <laurenanders92@gmail.com>
2017-04-30 21:55:02 +0100

    Create bstLevelOrderTraversal3.py

---
+++ 30DaysOfCode/day23/bstLevelOrderTraversal3.py
@@ -0,0 +1,46 @@
+import sys
+
+class Node:
+    def __init__(self, data):
+        self.right = self.left = None
+        self.data = data
+        
+class Solution:
+    def insert(self, root, data):
+        if root == None:
+            return Node(data)
+        else:
+            if data <= root.data:
+                cur = self.insert(root.left, data)
+                root.left = cur
+            else:
+                cur = self.insert(root.right, data)
+                root.right = cur
+        return root
+        
+    def levelOrder(self, root):
+        n = root
+        q = []
+        
+        while n:
+            print(n.data, end=' ')
+            
+            if n.left:
+                q.append(n.left)
+            if n.right:
+                q.append(n.right)
+                
+            if len(q) > 0:
+                n = q.pop(0)
+            else:
+                n = None
+                
+T = int(input())
+myTree = Solution()
+root = None
+
+for i in range(T):
+    data = int(input())
+    root = myTree.insert(root, data)
+    
+myTree.levelOrder(root)
