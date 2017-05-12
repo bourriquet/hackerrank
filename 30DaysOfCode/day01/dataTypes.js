9f3407f0b10f64e6a7a363fbb8339e61198035ce
Lauren Anders <laurenanders92@gmail.com>
2017-05-12 18:37:37 +0100

    Update dataTypes.js

--- 30DaysOfCode/day01/dataTypes.js
+++ 30DaysOfCode/day01/dataTypes.js
@@ -14,7 +14,6 @@ process.stdin.on('end', function () {
     main();    
 });
 
-// Reads complete line from STDIN
 function readLine() {
     return input_stdin_array[input_currentline++];
 }
@@ -24,22 +23,13 @@ function main() {
     var d = 4.0
     var s = "HackerRank "
     
-    // Declare second integer, double, and String variables.
     var another_i, another_d, another_s;
 
-    // Read and save an integer, double, and String to your variables.
     another_i = readLine();
     another_d = readLine();
     another_s = readLine();
 
-    // Print the sum of both integer variables on a new line.
     console.log(i + parseInt(another_i));
-
-    // Print the sum of the double variables on a new line.
     console.log((d + (parseFloat(another_d))).toFixed(1));
-
-    // Concatenate and print the String variables on a new line
-    // The 's' variable above should be printed first.
     console.log(s + another_s);
-    
 }
