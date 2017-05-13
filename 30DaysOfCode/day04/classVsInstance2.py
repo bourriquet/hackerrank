f56f2e58a45f004e90a96781a109c040962cb7f3
Lauren Anders <laurenanders92@gmail.com>
2017-05-13 12:27:46 +0100

    Update classVsInstance2.py

--- 30DaysOfCode/day04/classVsInstance2.py
+++ 30DaysOfCode/day04/classVsInstance2.py
@@ -1,6 +1,5 @@
 class Person:
     def __init__(self,initialAge):
-        # Add some more code to run some checks on initialAge
         if (initialAge < 0):
             self.age = 0
             print "Age is not valid, setting age to 0."
@@ -8,7 +7,6 @@ class Person:
             self.age = initialAge
             
     def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
         if (self.age < 13):
             print "You are young."
         elif (self.age >= 13 and self.age < 18):
@@ -17,7 +15,6 @@ class Person:
             print "You are old."
         
     def yearPasses(self):
-        # Increment the age of the person in here
         self.age += 1
         
 t = int(raw_input())
