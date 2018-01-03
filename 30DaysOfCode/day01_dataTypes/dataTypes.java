ae6264e476b3533f25d9b1e175e7af16fce02e69
Lauren Anders <laurenanders92@gmail.com>
2018-01-03 19:01:55 +0000

    Create dataTypes.java

---
+++ 30DaysOfCode/day01_dataTypes/dataTypes.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        
+        int another_i;
+        double another_d;
+        String another_s;
+
+        another_i = scan.nextInt();
+        another_d = scan.nextDouble();
+        scan.nextLine();
+        another_s = scan.nextLine();
+
+        System.out.println(i + another_i);
+		    System.out.println(d + another_d);
+        System.out.println(s + another_s);
+        
+        scan.close();
+    }
+}
