45090377a5c875b1149ad7013b075d3a332d20b5
Lauren Anders <laurenanders92@gmail.com>
2018-01-06 22:51:24 +0000

    Create introToConditionalStatements.java

---
+++ 30DaysOfCode/day03_introToConditionalStatements/introToConditionalStatements.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+   
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt(); 
+      scan.close();
+          
+      if (n % 2 == 1) {
+          System.out.println("Weird");
+      }
+      else{
+          if (n >= 2 && n <= 5) {
+              System.out.println("Not Weird");
+          }
+          else if (n >= 6 && n <= 20) {
+              System.out.println("Weird");
+          }
+          else {
+              System.out.println("Not Weird");
+          }
+      }
+   }
+}
