8ab6b2a1c284bbc703601d0bd2b2928b6f4e1aee
Lauren Anders <laurenanders92@gmail.com>
2018-01-22 22:34:55 +0000

    Create arrays.php

---
+++ 30DaysOfCode/day07_arrays/arrays.php
@@ -0,0 +1,13 @@
+<?php
+
+$handle = fopen ("php://stdin","r");
+fscanf($handle,"%d",$n);
+$arr_temp = fgets($handle);
+$arr = explode(" ",$arr_temp);
+array_walk($arr,'intval');
+
+for ($i = $n-1; $i >= 0; $i--) {
+    echo $arr[$i] . " ";
+}
+
+?>
