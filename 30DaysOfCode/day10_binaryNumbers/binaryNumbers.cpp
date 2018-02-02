5bb13d2ffed727262d1e216f25aabf71f9bf019f
Lauren Anders <laurenanders92@gmail.com>
2018-02-02 09:01:58 +0000

    Update binaryNumbers.cpp

--- 30DaysOfCode/day10_binaryNumbers/binaryNumbers.cpp
+++ 30DaysOfCode/day10_binaryNumbers/binaryNumbers.cpp
@@ -4,49 +4,28 @@
 
 using namespace std;
 
-string convertToBinary(int num) {
-    string numBin = "";
-    
-    while (num != 0) {
-        int r = num % 2;
-        
-        ostringstream convert;
-        convert << r;
-        numBin.insert(0, convert.str());
-        
-        num /= 2;
-    }
-    
-    return numBin;
-}
-
-int consecutiveOnes(int num) {
-    string numBin = convertToBinary(num);
-    
-    int count = 0;
-    int max = 0;
-    
-    for (int i = 0; i < numBin.length(); i++) {
-        if (numBin[i] == '1') {
-            count++;
-        }
-        else {
-            count = 0;
-        }
-        
-        if (count > max) {
-            max = count;
-        }
-    }
-    
-    return max;
-}
-
 int main(){
     int n;
     cin >> n;
     
-    cout << consecutiveOnes(n);
+    int max = 0;
+    int count = 0;
+    
+    while (n > 0) {
+        if (n % 2 == 1) {
+            count++;
+            if (count > max) {
+                max = count;
+            }
+        } else {
+            count = 0;
+        }
+        
+        n /= 2;
+    }
+    
+    cout << max;
     
     return 0;
 }
+
