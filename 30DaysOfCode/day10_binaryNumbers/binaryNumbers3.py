36129a3fdf4947b06505a92fd3c5e64f5271a3aa
Lauren Anders <laurenanders92@gmail.com>
2018-02-03 22:43:49 +0000

    Update binaryNumbers3.py

--- 30DaysOfCode/day10_binaryNumbers/binaryNumbers3.py
+++ 30DaysOfCode/day10_binaryNumbers/binaryNumbers3.py
@@ -1,32 +1,19 @@
 #!/bin/python3
 
-def convert_to_binary(num):
-    num_bin = ""
-    
-    while num != 0:
-        r = num % 2
-        num_bin += str(r)
-        num = int(num / 2)
-        
-    return num_bin
-        
-def consecutive_ones(num):
-    num_bin = convert_to_binary(num)
-    
-    count = 0
-    count_max = 0
-    
-    for i in range(0, len(num_bin)):
-        if num_bin[i] == '1':
-            count += 1
-        else:
-            count = 0
-        
-        if count > count_max:
-            count_max = count
-            
-    return count_max
-    
+import sys
 
 n = int(input().strip())
-print(consecutive_ones(n))
+
+count = 0
+maxCount = 0
+
+while n > 0:
+    if n % 2 == 1:
+        count += 1
+        if count > maxCount:
+            maxCount = count
+    else:
+        count = 0
+    n = int(n / 2)
+    
+print(maxCount)
