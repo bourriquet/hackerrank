e9b2466851f6b6e74353d1867c63e5f3852373b4
Lauren Anders <laurenanders92@gmail.com>
2018-02-11 23:22:15 +0000

    Update inheritance.cs

--- 30DaysOfCode/day12_inheritance/inheritance.cs
+++ 30DaysOfCode/day12_inheritance/inheritance.cs
@@ -2,19 +2,19 @@ using System;
 using System.Linq;
 
 class Person {
-	protected string firstName;
-	protected string lastName;
-	protected int id;
+    protected string firstName;
+    protected string lastName;
+    protected int id;
 	
-	public Person() {}
-	public Person(string firstName, string lastName, int identification) {
-			this.firstName = firstName;
-			this.lastName = lastName;
-			this.id = identification;
-	}
-	public void printPerson() {
-		Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id); 
-	}
+    public Person() {}
+    public Person(string firstName, string lastName, int identification) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = identification;
+    }
+    public void printPerson() {
+        Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id); 
+    }
 }
 
 class Student : Person {
@@ -46,20 +46,22 @@ class Student : Person {
 }
 
 class Solution {
-	static void Main() {
-		string[] inputs = Console.ReadLine().Split();
-		string firstName = inputs[0];
-	  	string lastName = inputs[1];
-		int id = Convert.ToInt32(inputs[2]);
-		int numScores = Convert.ToInt32(Console.ReadLine());
-		inputs = Console.ReadLine().Split();
-	  	int[] scores = new int[numScores];
-		for(int i = 0; i < numScores; i++){
-			scores[i]= Convert.ToInt32(inputs[i]);
-		} 
+    static void Main() {
+        string[] inputs = Console.ReadLine().Split();
+        string firstName = inputs[0];
+        string lastName = inputs[1];
+        int id = Convert.ToInt32(inputs[2]);
+        int numScores = Convert.ToInt32(Console.ReadLine());
+	    
+        inputs = Console.ReadLine().Split();
+        int[] scores = new int[numScores];
+	    
+        for(int i = 0; i < numScores; i++){
+            scores[i]= Convert.ToInt32(inputs[i]);
+        } 
 	  	
-		Student s = new Student(firstName, lastName, id, scores);
-		s.printPerson();
-		Console.WriteLine("Grade: " + s.Calculate() + "\n");
-	}
+        Student s = new Student(firstName, lastName, id, scores);
+        s.printPerson();
+        Console.WriteLine("Grade: " + s.Calculate() + "\n");
+    }
 }
