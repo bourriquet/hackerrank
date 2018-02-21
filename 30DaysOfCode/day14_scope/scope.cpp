269237f96789667f4c9e66bafbdad5aa21bcd2c0
Lauren Anders <laurenanders92@gmail.com>
2018-02-21 23:11:05 +0000

    Update scope.cpp

--- 30DaysOfCode/day14_scope/scope.cpp
+++ 30DaysOfCode/day14_scope/scope.cpp
@@ -7,28 +7,28 @@
 using namespace std;
 
 class Difference {
-    private:
-      vector<int> elements;
+  private:
+    vector<int> elements;
   
-  	public:
-  	  int maximumDifference;
+  public:
+    int maximumDifference;
     
-    	Difference(vector<int> a) {
-        elements = a;
+    Difference(vector<int> a) {
+      elements = a;
     }
 
     void computeDifference() {
-        int minNum = 10;
-        int maxNum = 1;
+      int minNum = 10;
+      int maxNum = 1;
         
-        for (int i = 0; i < elements.size(); i++) {
-            if (elements[i] < minNum)
-                minNum = elements[i];
-            if (elements[i] > maxNum)
-                maxNum = elements[i];
-        }
+      for (int i = 0; i < elements.size(); i++) {
+        if (elements[i] < minNum)
+          minNum = elements[i];
+        if (elements[i] > maxNum)
+          maxNum = elements[i];
+      }
         
-        maximumDifference = (maxNum - minNum);
+      maximumDifference = (maxNum - minNum);
     }
 };
 
