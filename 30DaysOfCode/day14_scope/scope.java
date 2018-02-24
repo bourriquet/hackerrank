cfc49a59cc10f2b83e57bc9ee700efbda43ea926
Lauren Anders <laurenanders92@gmail.com>
2018-02-24 22:53:40 +0000

    Create scope.java

---
+++ 30DaysOfCode/day14_scope/scope.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+    
+    Difference(int[] elems) {
+        elements = elems;
+        Arrays.sort(elements);
+    }
+
+    public void computeDifference() {
+        maximumDifference = Math.abs(elements[elements.length - 1] - elements[0]);
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
