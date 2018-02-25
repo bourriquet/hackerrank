ba8135d09321ea65adebab6a9218828f04bff59b
Lauren Anders <laurenanders92@gmail.com>
2018-02-25 22:45:12 +0000

    Create scope.php

---
+++ 30DaysOfCode/day14_scope/scope.php
@@ -0,0 +1,23 @@
+<?php
+class Difference{
+    private $elements=array();
+    public $maximumDifference;
+
+    function __construct($elems) {
+        $this->elements = $elems;
+        sort($this->elements);
+    }
+
+    function ComputeDifference() {
+        $this->maximumDifference = abs($this->elements[count($this->elements) - 1] - $this->elements[0]);
+    }
+}
+
+$N=intval(fgets(STDIN));
+$a =array_map('intval', explode(' ', fgets(STDIN)));
+
+$d=new Difference($a);
+$d->ComputeDifference();
+
+print ($d->maximumDifference);
+?>
