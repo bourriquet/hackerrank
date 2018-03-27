0a86b6739ff138d69d3ce51e5c67359018b79d75
Lauren Anders <laurenanders92@gmail.com>
2018-03-27 17:51:53 +0100

    Update sorting.cpp

--- 30DaysOfCode/day20_sorting/sorting.cpp
+++ 30DaysOfCode/day20_sorting/sorting.cpp
@@ -13,12 +13,13 @@ int main() {
     int n;
     cin >> n;
     vector<int> a(n);
-    int numSwaps = 0;
     
     for(int a_i = 0; a_i < n; a_i++){
        cin >> a[a_i];
     }
 
+    int numSwaps = 0;
+    
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (a[j] > a[j+1]) {
