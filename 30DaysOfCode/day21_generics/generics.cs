385000a4c397b06db1592ddf8f77aea5d292a4be
Lauren Anders <laurenanders92@gmail.com>
2018-04-01 22:39:17 +0100

    Update generics.cs

--- 30DaysOfCode/day21_generics/generics.cs
+++ 30DaysOfCode/day21_generics/generics.cs
@@ -8,21 +8,21 @@ class Printer {
     }
     
     static void Main(string[] args) {
-		    int n = Convert.ToInt32(Console.ReadLine());
-		    int[] intArray = new int[n];
+	int n = Convert.ToInt32(Console.ReadLine());
+	int[] intArray = new int[n];
 		
         for (int i = 0; i < n; i++) {
-			      intArray[i] = Convert.ToInt32(Console.ReadLine());
-		    }
+	    intArray[i] = Convert.ToInt32(Console.ReadLine());
+	}
 		
-		    n = Convert.ToInt32(Console.ReadLine());
-		    string[] stringArray = new string[n];
+	n = Convert.ToInt32(Console.ReadLine());
+	string[] stringArray = new string[n];
 		
         for (int i = 0; i < n; i++) {
-			      stringArray[i] = Console.ReadLine();
-		    }
+	    stringArray[i] = Console.ReadLine();
+	}
 		
-		    PrintArray<Int32>(intArray);
-		    PrintArray<String>(stringArray);
-	  }
+	PrintArray<Int32>(intArray);
+	PrintArray<String>(stringArray);
+    }
 }
