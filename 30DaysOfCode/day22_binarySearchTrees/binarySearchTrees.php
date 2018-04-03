24a0488435d46086af7f3d620a0414775c288668
Lauren Anders <laurenanders92@gmail.com>
2018-04-03 22:20:19 +0100

    Create binarySearchTrees.php

---
+++ 30DaysOfCode/day22_binarySearchTrees/binarySearchTrees.php
@@ -0,0 +1,46 @@
+<?php
+class Node {
+    public $left, $right;
+    public $data;
+    function __construct($data) {
+        $this->left = $this->right = null;
+        $this->data = $data;
+    }
+}
+
+class Solution {
+    public function insert($root, $data) {
+        if ($root == null) {
+            return new Node($data);
+        } else {
+            if($data <= $root->data) {
+                $cur = $this->insert($root->left, $data);
+                $root->left = $cur;
+            } else {
+                $cur = $this->insert($root->right, $data);
+                $root->right = $cur;
+            }
+            return $root;
+        }
+    }
+    
+    public function getHeight($root){
+        if ($root == null)
+            return -1;
+        
+        return max($this->getHeight($root->left), $this->getHeight($root->right)) + 1;
+    }   
+}
+
+$myTree = new Solution();
+$root = null;
+$T = intval(fgets(STDIN));
+
+while ($T-- > 0) {
+    $data = intval(fgets(STDIN));
+    $root = $myTree->insert($root, $data);
+}
+
+$height = $myTree->getHeight($root);
+echo $height;
+?>
