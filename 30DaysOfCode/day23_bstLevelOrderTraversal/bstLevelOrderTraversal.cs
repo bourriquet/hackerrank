7bf78f43d2855c7af8d5ccbac6eb2864bc96899d
Lauren Anders <laurenanders92@gmail.com>
2018-04-04 21:58:27 +0100

    Create bstLevelOrderTraversal.cs

---
+++ 30DaysOfCode/day23_bstLevelOrderTraversal/bstLevelOrderTraversal.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Node {
+    public Node left, right;
+    public int data;
+    
+    public Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+class Solution {
+    static void levelOrder(Node root){
+        List<Node> q = new List<Node>();
+        q.Add(root);
+        
+        while (q.Count > 0) {
+            Console.Write(q[0].data + " ");
+            if (q[0].left != null)
+                q.Add(q[0].left);
+            if (q[0].right != null)
+                q.Add(q[0].right);
+            
+            if (q.Count > 0) {
+                q.RemoveAt(0);
+            }
+        }
+    }
+    
+    static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+    
+    static void Main(String[] args) {
+        Node root = null;
+        int T = Int32.Parse(Console.ReadLine());
+        
+        while (T-- > 0) {
+            int data = Int32.Parse(Console.ReadLine());
+            root = insert(root, data);            
+        }
+        levelOrder(root);
+    }
+}
