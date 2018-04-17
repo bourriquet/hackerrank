e1a0a24c704a0d745b63f3bcb35a585403ce6b8d
Lauren Anders <laurenanders92@gmail.com>
2018-04-17 22:46:35 +0100

    Create runningTimeAndComplexity.cpp

---
+++ 30DaysOfCode/day25_runningTimeAndComplexity/runningTimeAndComplexity.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        bool flag = true;
+        
+        if (num < 2) {
+            cout << "Not prime" << endl;
+        } else if (num == 2) {
+            cout << "Prime" << endl;
+        } else if (num % 2 == 0) {
+            cout << "Not prime" << endl;
+        } else {
+            for (int i = 3; i < num/2; i += 2) {
+                if (num % i == 0) {
+                    cout << "Not prime" << endl;
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if (flag) {
+                cout << "Prime" << endl;
+            }
+        }
+    }
+    
+    return 0;
+}
