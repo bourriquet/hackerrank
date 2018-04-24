916d30e84d85fd55b65bed2f6f94bb7666fc5c72
Lauren Anders <laurenanders92@gmail.com>
2018-04-24 22:36:06 +0100

    Create runningTimeAndComplexity2.py

---
+++ 30DaysOfCode/day25_runningTimeAndComplexity/runningTimeAndComplexity2.py
@@ -0,0 +1,23 @@
+from math import sqrt, ceil
+
+n = int(input())
+
+for i in range(1, (n + 1)):
+    num = int(input())
+    flag = True
+    
+    if (num < 2):
+        print "Not prime"
+    elif (num <= 3):
+        print "Prime"
+    elif (num % 2 == 0 or num % 3 == 0):
+        print "Not prime"
+    else:
+        for j in range(5, int(ceil(sqrt(num))) + 1, 2):
+            if (num % j == 0):
+                print "Not prime"
+                flag = False
+                break
+        
+        if flag:
+            print "Prime"
