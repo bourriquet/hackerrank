7f7f93ed1e962d89259f81b418cc8b468c33f7ca
Lauren Anders <laurenanders92@gmail.com>
2018-06-14 21:33:23 +0100

    Create endOfFile.java

---
+++ java/introduction/endOfFile.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int i = 1;
+        while (scanner.hasNextLine()) {
+            System.out.println(i + " " + scanner.nextLine());
+            i++;
+        }
+    }
+}
